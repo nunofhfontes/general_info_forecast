edda1024b6309e1a874bb4d7a8153a7979770a42
nunofhfontes <nunofhfontes@gmail.com>
2022-02-21 19:47:56 +0000

    reading json files

--- utils/files/fileUtils.go
+++ utils/files/fileUtils.go
@@ -1,5 +1,26 @@
 package fileUtils
 
+import (
+	"fmt"
+	"os"
+)
+
 func ReadLocations() {
 	println("reading json locations files")
+
+	// Open our jsonFile
+	jsonFile, err := os.Open("./data/locations.json")
+
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened locations.json")
+
+	// print file content
+	println(jsonFile.)
+
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
 }
