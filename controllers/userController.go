e8bc41c9eaf90632ab9109968a16fd0a60de2d04
nunofontes <nunof.h.fontes@gmail.com>
2022-03-13 00:11:28 +0000

    userController caling authUtils with mock data - incomplete

--- controllers/userController.go
+++ controllers/userController.go
@@ -4,10 +4,24 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+
+	authUtils "dataForecast/utils/auth"
 )
 
 func Login(c *fiber.Ctx) error {
 	fmt.Println("Should do Login")
+
+	user := c.FormValue("user")
+	pass := c.FormValue("password")
+	println("user: ", user)
+	println("password: ", pass)
+
+	// 1 - get data from DB
+
+	// 2 - put it into a structure
+
+	// get an access toke (jwt) with the submited info
+	authUtils.CreateJwt()
 
 	// For testing purposes- testing centralized Error Handling
 	// 503 On vacation!
