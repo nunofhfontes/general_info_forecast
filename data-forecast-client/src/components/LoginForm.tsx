c0f268ed9c902a3a431559d278189d66b3510115
nunofontes <nunof.h.fontes@gmail.com>
2022-03-23 00:58:14 +0000

    Simple Login Form added and auth file removed

--- data-forecast-client/src/components/LoginForm.tsx
+++ data-forecast-client/src/components/LoginForm.tsx
@@ -5,12 +5,22 @@ interface Props {
 
 const LoginForm: React.FC<Props> = () => {
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" placeholder="name"/>
             <input type="text" placeholder="address" />
             <button> Submit </button>
         </form>
     )
+};
+
+const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log("Trying to submit the form");
+};
+
+const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    //setName(e.target.value);
 };
 
 export default LoginForm;
