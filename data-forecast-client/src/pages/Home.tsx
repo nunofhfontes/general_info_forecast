5f578fa3aae9b3479b32e55fb161711e42a9f7de
nunofontes <nunof.h.fontes@gmail.com>
2022-03-26 17:25:57 +0000

    Home Component Navbar style update

--- data-forecast-client/src/pages/Home.tsx
+++ data-forecast-client/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Container, Navbar } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 
@@ -16,10 +17,24 @@ const Home: React.FC<Props> = () => {
     };
 
     return(
-        <>
+        <div className="homePage vh-100 grey-background">
+            <Navbar className="mb-2" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand>
+                        <img
+                        alt=""
+                        src="/logo.svg"
+                        width="30"
+                        height="30"
+                        className="navbar-logo d-inline-block align-top"
+                        />{' '}
+                         Data Forecast App
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
             Home Page
             <button onClick={goHome}> Go Home </button>
-        </>
+        </div>
     )
 }
 export default Home;
