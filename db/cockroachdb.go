a2137763e13c9cd8eabae1995658ce5b600cf7ce
Nuno Fontes <nunof.h.fontes@gmail.com>
2022-08-08 22:45:34 +0100

    testing queries using XORM

--- db/cockroachdb.go
+++ db/cockroachdb.go
@@ -39,4 +39,37 @@ func InitCockroachDB(config fiberConfig.Config) {
 
 	fmt.Printf("count: " + strconv.FormatInt(counts, 10) + "\n")
 
+	// try to insert a user
+	fmt.Printf("\ntrying to insert a user \n\n")
+
+	type User struct {
+		Id       int64
+		Username string
+		//Name     string
+		//Salt     string
+		//Age      int
+		Password string `xorm:"varchar(200)"`
+		//AccountId int64
+		//Created  time.Time `xorm:"created"`
+		//Updated  time.Time `xorm:"updated"`
+	}
+
+	var existingUser User
+
+	has, err := engine.Get(&existingUser)
+	// SELECT * FROM user LIMIT 1
+	fmt.Printf("\n Has user?: %t \n\n", has)
+
+	fmt.Printf("\n Has user? existing user ->   %s \n\n", existingUser.Username)
+
+	has1, err := engine.Where("username = ?", "nunofhfontes").Desc("id").Get(&existingUser)
+	// SELECT * FROM user WHERE name = ? ORDER BY id DESC LIMIT 1
+	fmt.Printf("\n Has user1?: %t \n\n", has1)
+
+	fmt.Printf("\n Has user1? existing user ->   %s \n\n", existingUser.Password)
+
+	user := User{Username: "xiaoxiao", Password: "lunny"} // , Created: time.Now()
+	affected, err := engine.Insert(&user)
+
+	fmt.Printf("\n Affected rows: %d \n\n", affected)
 }
